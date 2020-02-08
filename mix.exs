bfa43fcdf564b8cc61eb0f7a2b9d1c90395a9d14
Barrett Helms <barrett.helms@salesloft.com>
2020-02-07 21:13:49 -0500

    Bumps version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Migrant.MixProject do
   def project do
     [
       app: :migrant,
-      version: "0.1.10",
+      version: "0.1.11",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
